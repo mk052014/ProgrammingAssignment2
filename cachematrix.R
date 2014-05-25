743ebd414c39da67f24be785d2299ce771122cb8
mk052014 <mk052014@users.noreply.github.com>
2014-05-24 23:12:57 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 #
 # This function calculates the inverse of the matrix. 
-# It first checks to see if the inverse  has already 
+# It first checks to see if the inverse of the matrix  has already 
 # been calculated. If so, it gets the inverse of the matrix 
 # from the cache and skips the computation. Otherwise, 
 # it calculates the inverse of the matrix and sets this value  
@@ -20,3 +20,22 @@ makeCacheMatrix <- function(x = matrix()) {
        getinv = getinv)
 }
 
+
+# CacheSolve function Returns cached matrix inverse using previously computed matrix
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinv()  # get the inverse of the matrix from cache
+  # check the value of m is NULL or not
+  # If the inverse of the matrix is not null, get it from the cache
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  # If the inverse of the matrix from the cache  is null, then generate the 
+  # inverse of the matrix 
+  #
+  data <- x$getinv()  # returns the original matrix
+  m <- solve(data, ...)  # creates the inverse of the matrix
+  x$setinv(m)  # save it in the cache
+  m #It displays the inverse of the matrix
+}
